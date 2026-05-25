c293f4bf63fdd0d038192ff7606b422109999e27
Joao R Q Neumann <joaorqn@gmail.com>
2026-05-25 14:10:32 -0300

    criacao pagamento-views

--- apps/pagamento/views.py
+++ apps/pagamento/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Pagamento
+from .serializer import PagamentoSerializer
 
-# Create your views here.
+class PagamentoViewSet(viewsets.ModelViewSet):
+    queryset = Pagamento.objects.all()
+    serializer_class = PagamentoSerializer
