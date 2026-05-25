ad19056d2bb318c3d03267ba858a7574b6e66e8a
Joao R Q Neumann <joaorqn@gmail.com>
2026-05-25 01:48:10 -0300

    criacao endereco-views

--- apps/endereco/views.py
+++ apps/endereco/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Endereco
+from .serializer import EnderecoSerializer
 
-# Create your views here.
+class EnderecoViewSet(viewsets.ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer 
