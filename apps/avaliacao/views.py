cf6b4c5fc84b7d90d54b78c6d2547b847311b11a
Joao R Q Neumann <joaorqn@gmail.com>
2026-05-25 15:26:36 -0300

    criacao avaliacao-views

--- apps/avaliacao/views.py
+++ apps/avaliacao/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Avaliacao
+from .serializer import AvaliacaoSerializer
 
-# Create your views here.
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer 
