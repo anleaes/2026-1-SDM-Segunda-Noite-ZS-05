80c4ccc8e86e7c524fd542db8195d808b56781f6
Joao R Q Neumann <joaorqn@gmail.com>
2026-05-25 14:10:48 -0300

    finalizacao pagamento-views

--- apps/pagamento/views.py
+++ apps/pagamento/views.py
@@ -6,3 +6,5 @@ from .serializer import PagamentoSerializer
 class PagamentoViewSet(viewsets.ModelViewSet):
     queryset = Pagamento.objects.all()
     serializer_class = PagamentoSerializer
+
+    
