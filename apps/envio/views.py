7acb51972c5e5e0705b131394ec110ad9971f7da
Joao R Q Neumann <joaorqn@gmail.com>
2026-05-25 14:37:14 -0300

    criacao envio-views

--- apps/envio/views.py
+++ apps/envio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Envio
+from .serializer import EnvioSerializer
 
-# Create your views here.
+class EnvioViewSet(viewsets.ModelViewSet):
+    queryset = Envio.objects.all()
+    serializer_class = EnvioSerializer
