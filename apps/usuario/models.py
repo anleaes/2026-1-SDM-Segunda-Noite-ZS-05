a09704b8267891ed79644297ec205305eb4db981
Joao R Q Neumann <joaorqn@gmail.com>
2026-05-25 01:15:05 -0300

    criacao usuario-models

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from pessoa.models import Pessoa
 
-# Create your models here.
+class Usuario(models.Model):
+    PERFIL_CHOICES = [
+        ('comprador', 'Comprador'),
+        ('vendedor', 'Vendedor'),
+        ('admin', 'Administrador'),
+    ]
+
+    ativo = models.BooleanField(default=True)
+    pontuacao = models.IntegerField(default=0)
+    cpf = models.CharField(max_length=14, unique=True)
+    perfil = models.CharField(max_length=20, choices=PERFIL_CHOICES)
+    pessoa = models.OneToOneField(Pessoa, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return f'Usuario: {self.pessoa.nome} - {self.perfil}'
