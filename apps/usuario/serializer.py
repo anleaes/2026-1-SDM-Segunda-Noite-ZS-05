f69a1a89027cf07515bc90a38818d6054cdcfa67
Nicolas-sa-oliveira <Nicolas-sa-oliveira@hotmail.com>
2026-06-08 13:40:04 -0300

    Ajuste no serialzier do usuario

--- apps/usuario/serializer.py
+++ apps/usuario/serializer.py
@@ -1,9 +1,11 @@
-from .models import Usuario
 from rest_framework import serializers
+from .models import Usuario
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    # Estas duas linhas fazem a mágica de buscar o nome na tabela Pessoa buscando pela FK
+    pessoa_nome = serializers.CharField(source='pessoa.nome', read_only=True)
+    pessoa_sobrenome = serializers.CharField(source='pessoa.sobrenome', read_only=True)
+
     class Meta:
         model = Usuario
-        fields = '__all__'
-
-        
+        fields = '__all__' # Mantém o __all__ para trazer o CPF, perfil, ativo, etc.
