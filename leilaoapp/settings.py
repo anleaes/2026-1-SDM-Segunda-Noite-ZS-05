8369cbb5a49088b796719a6354d67a4ab928da37
Nicolas-sa-oliveira <Nicolas-sa-oliveira@hotmail.com>
2026-06-08 11:46:54 -0300

    CORS aplicado na main!!

--- leilaoapp/settings.py
+++ leilaoapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'categoria.apps.CategoriaConfig',
     'pessoa.apps.PessoaConfig',
     'usuario.apps.UsuarioConfig',
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +133,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
