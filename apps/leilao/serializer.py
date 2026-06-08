24486f0c14c92a083e716aba159ae87eb7843eb0
Joao R Q Neumann <joaorqn@gmail.com>
2026-06-08 17:54:41 -0300

    Ajustes Serializer

--- apps/leilao/serializer.py
+++ apps/leilao/serializer.py
@@ -1,9 +1,9 @@
-from .models import Leilao
 from rest_framework import serializers
+from .models import Leilao
 
 class LeilaoSerializer(serializers.ModelSerializer):
+    produto_nome = serializers.CharField(source='produto.nome', read_only=True)
+
     class Meta:
         model = Leilao
         fields = '__all__'
-
-        
--- apps/produto/serializer.py
+++ apps/produto/serializer.py
@@ -1,9 +1,10 @@
-from .models import Produto
 from rest_framework import serializers
+from .models import Produto
 
 class ProdutoSerializer(serializers.ModelSerializer):
+    categoria_nome = serializers.CharField(source='categoria.nome', read_only=True)
+    vendedor_nome = serializers.CharField(source='vendedor.pessoa.nome', read_only=True)
+
     class Meta:
         model = Produto
         fields = '__all__'
-
-        
